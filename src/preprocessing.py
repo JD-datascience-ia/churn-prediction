66a8ba31e301af09498b1bfc9a8b38c23781e79c
Julien Deboté <julien.debote34170@gmail.com>
2026-04-09 03:20:17 +0200

    mis een place features engineering et utilisation des modèles

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,2 +1,54 @@
-pass
+import pandas as pd
+
+
+def add_tenure_group(df):
+    
+    df["tenure_group"] = pd.cut(
+        df["tenure"],
+        bins=[0, 12, 24, 48, 72],
+        labels=["0-1y", "1-2y", "2-4y", "4-6y"]
+    )
+    
+    return df
+
+def add_charge_level(df):
+    
+    df["high_charges"] = (df["MonthlyCharges"] > df["MonthlyCharges"].median()).astype(int)
+    
+    return df
+
+def add_engagement_score(df):
+    
+    df["engagement_score"] = df["tenure"] * df["MonthlyCharges"]
+    
+    return df
+
+def add_service_count(df):
+    
+    services = [
+        "InternetService",
+        "OnlineSecurity",
+        "OnlineBackup",
+        "DeviceProtection",
+        "TechSupport",
+        "StreamingTV",
+        "StreamingMovies"
+    ]
+    
+    df["service_count"] = df[services].apply(lambda x: (x == "Yes").sum(), axis=1)
+    
+    return df
+
+def feature_engineering(df):
+    df = add_tenure_group(df)
+    df = add_charge_level(df)
+    df = add_engagement_score(df)
+    df = add_service_count(df)
+    
+    return df
+
+
+
+
+
 
